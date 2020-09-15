144085297e76594421bb2dc930fcd3a7c281f432
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 16:47:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  counter = 1 
+  counter = 0
   board.each do |space|
     if space == "X" || space == "O"
       counter += 1
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 != 0
+  if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
